b4041f115f2189e611ba38fef4cf56d2df34b71a
Kartik Agarwal <kartikhustles@gmail.com>
2024-03-15 03:43:27 +0530

    Frontend Updated

--- Frontend/src/pages/Dashboard.js
+++ Frontend/src/pages/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import CoolButton from "../components/Button/CoolButton";
 import { Link } from "react-router-dom";
+import VideoUploader from "../components/VideoUploadForm";
 
 const Dashboard = () => {
   return (
     <div className="flex justify-center items-center text-3xl h-full">
+      {/* <VideoUploader /> */}
       <Link to="/traffic">
         <CoolButton />
       </Link>
