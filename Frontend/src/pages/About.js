c94db00983036660af9ece7f67f9a40111559fac
Kartik Agarwal <kartikhustles@gmail.com>
2024-03-16 01:18:23 +0530

    About Section Updated

--- Frontend/src/pages/About.js
+++ Frontend/src/pages/About.js
@@ -1,6 +1,6 @@
 import React from "react";
-import frameImage from "../assets/frame.png";
-import image from "../assets/about.png";
+import frameImage from "../assets/frame-about.png";
+import image from "../assets/Pic-about.png";
 import OurVision from "../assets/OurVision.png";
 import OurMission from "../assets/OurMission.png";
 import { Link } from "react-router-dom";
@@ -8,33 +8,28 @@ import { Link } from "react-router-dom";
 const About = ({ isLoggedIn }) => {
   return (
     <div className="flex-col mx-12 my-6">
-      <div className="flex">
+      <div className="flex mt-10 mb-20 mx-6">
         <div className="w-11/12 h-full my-auto">
-          <div className="text-3xl font-bold">How It Works</div>
+          <div className="text-3xl font-bold">How ClearPath Works?</div>
           <div className="text-xl font-medium mr-10 mt-6">
-            Our Dynamic Traffic Signaling system intelligently reallocates
-            signal times based on traffic density, redistributing time to lanes
-            where congestion is highest. Meanwhile, our Emergency Vehicle
-            Detection technology utilizes audio and video cues to detect and
-            prioritize the passage of emergency vehicles. By seamlessly
-            integrating these solutions, ClearPath ensures efficient and
-            uninterrupted emergency response.
+            ClearPath is a technology that aims to revolutionize emergency
+            response systems and optimize urban traffic management.Our Dynamic
+            Traffic Signaling system intelligently reallocates signal times
+            based on traffic density, redistributing time to lanes where
+            congestion is highest. Meanwhile, our Emergency Vehicle Detection
+            technology accurately detects and prioritizes the passage of
+            emergency vehicles by reallocating time to lanes wherever an
+            emergency vehicle is detected. By seamlessly integrating these
+            solutions, ClearPath ensures efficient and uninterrupted emergency
+            response.
           </div>
         </div>
         <div className="relative w-11/12 max-w-[450px] ">
-          <img
-            src={frameImage}
-            alt="Pattern"
-            width={558}
-            height={504}
-            loading="lazy"
-          />
+          <img src={frameImage} alt="Pattern" loading="lazy" />
 
           <img
             src={image}
             alt="template hero image"
-            width={558}
-            height={490}
             loading="lazy"
             className="absolute -top-4 right-4"
           />
@@ -43,7 +38,7 @@ const About = ({ isLoggedIn }) => {
       <div className="flex mt-12">
         <div className="flex-col m-6">
           <div className="flex">
-            <div>
+            <div className="my-2 justify-center">
               <img
                 src={OurMission}
                 alt="template hero image"
@@ -53,7 +48,7 @@ const About = ({ isLoggedIn }) => {
                 className="mr-2"
               />
             </div>
-            <div className="text-3xl font-bold my-2">Our Vision</div>
+            <div className="text-3xl font-bold my-2">Our Mission</div>
           </div>
           <div className="text-lg font-medium">
             Be one of the top analytics consultancy organizations which operates
@@ -61,7 +56,7 @@ const About = ({ isLoggedIn }) => {
             its stakeholders.
           </div>
         </div>
-        <div className="flex-col m-6">
+        <div className="flex-col m-6 justify-center">
           <div className="flex">
             <div>
               <img
@@ -86,38 +81,44 @@ const About = ({ isLoggedIn }) => {
         <div className="text-3xl text-center my-10 font-bold">
           Meet the minds behind ClearPath
         </div>
-        <div className="flex flex-wrap justify-center m-4">
-          <div className="flex-col m-10 max-w-xl border-4 rounded-3xl">
-            <img
-              src="https://avatars.githubusercontent.com/u/72240452?v=4"
-              alt="profile image"
-              width={100}
-              height={100}
-              loading="lazy"
-              className="rounded-full my-2 mx-auto"
-            />
-            <div className="text-4xl my-2 text-center font-semibold">
-              Samarth Asthana
-            </div>
-            <div className="text-2xl my-2 text-center font-medium">AI/ML</div>
-            <div className="text-lg m-4 text-center w-96 font-medium ">
-              PhD University of Colorado, USA. Expert in mathematical modeling
-              with more than 20 years of research and development experience in
-              worldclass research organizations both in India and the USA.
-            </div>
-            <div>
-              <Link to="https://www.linkedin.com/in/samarth-asthana/">
-                <img
-                  src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png"
-                  alt="LinkedIn Icon"
-                  width={50}
-                  height={50}
-                  loading="lazy"
-                  className="my-2 mx-auto"
-                />
-              </Link>
+        <div className="text-2xl text-center my-4 font-bold">Team Lead</div>
+        <div>
+          <div className="flex flex-wrap justify-center m-4">
+            <div className="flex-col m-10 max-w-lg border-4 rounded-3xl">
+              <img
+                src="https://avatars.githubusercontent.com/u/72240452?v=4"
+                alt="profile image"
+                width={100}
+                height={100}
+                loading="lazy"
+                className="rounded-full my-2 mx-auto"
+              />
+              <div className="text-4xl my-2 text-center font-semibold">
+                Samarth Asthana
+              </div>
+              <div className="text-2xl my-2 text-center font-medium">AI/ML</div>
+              <div className="text-lg m-4 text-center w-72 font-medium ">
+                PhD University of Colorado, USA. Expert in mathematical modeling
+                with more than 20 years of research and development experience
+                in worldclass research organizations both in India and the USA.
+              </div>
+              <div>
+                <Link to="https://www.linkedin.com/in/samarth-asthana/">
+                  <img
+                    src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png"
+                    alt="LinkedIn Icon"
+                    width={50}
+                    height={50}
+                    loading="lazy"
+                    className="my-2 mx-auto"
+                  />
+                </Link>
+              </div>
             </div>
           </div>
+        </div>
+        <div className="text-2xl text-center my-4 font-bold">Team Members</div>
+        <div className="flex flex-wrap justify-center m-4">
           <div className="flex-col m-10 max-w-xl border-4 rounded-3xl">
             <img
               src="https://media.licdn.com/dms/image/D4D03AQFkKdHGv2S2VQ/profile-displayphoto-shrink_400_400/0/1701117745768?e=1715817600&v=beta&t=U71hS-LqMvZPPceUK5L-GpOAtIxkcPmGLftfHw3kwgs"
@@ -131,7 +132,7 @@ const About = ({ isLoggedIn }) => {
               Arihant Bharadwaj
             </div>
             <div className="text-2xl my-2 text-center font-medium">AI/ML</div>
-            <div className="text-lg m-4 text-center w-96 font-medium ">
+            <div className="text-lg m-4 text-center w-72 font-medium ">
               PhD University of Colorado, USA. Expert in mathematical modeling
               with more than 20 years of research and development experience in
               worldclass research organizations both in India and the USA.
@@ -162,7 +163,7 @@ const About = ({ isLoggedIn }) => {
               Kartik Agarwal
             </div>
             <div className="text-2xl my-2 text-center font-medium">AI/ML</div>
-            <div className="text-lg m-4 text-center w-96 font-medium ">
+            <div className="text-lg m-4 text-center w-72 font-medium ">
               PhD University of Colorado, USA. Expert in mathematical modeling
               with more than 20 years of research and development experience in
               worldclass research organizations both in India and the USA.
@@ -193,7 +194,7 @@ const About = ({ isLoggedIn }) => {
               Ayush Patil
             </div>
             <div className="text-2xl my-2 text-center font-medium">AI/ML</div>
-            <div className="text-lg m-4 text-center w-96 font-medium ">
+            <div className="text-lg m-4 text-center w-72 font-medium ">
               PhD University of Colorado, USA. Expert in mathematical modeling
               with more than 20 years of research and development experience in
               worldclass research organizations both in India and the USA.
